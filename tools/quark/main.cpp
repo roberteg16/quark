33b1184e67cc9e394f476344ab2ecc4fd4d22503
Robert Esclapez <robertesclapez@gmail.com>
2021-06-29 20:33:06 +0200

    [QK] Update tool

--- tools/quark/main.cpp
+++ tools/quark/main.cpp
@@ -1,8 +1,38 @@
+#include <llvm-12/llvm/Support/Debug.h>
+#include <quark/Frontend/LexContext.h>
 #include <quark/Frontend/SourceModule.h>
 
-int main(int argc, const char *arv[]) {
-  quark::SourceModule SM;
-  SM.dump();
+#include "../../build/lib/Frontend/QuarkParser.hpp"
+#include <llvm/Support/InitLLVM.h>
+
+#include <fstream>
+
+using namespace quark;
+
+void quark::QuarkParser::error(const std::string &error) {
+  llvm::outs() << "Error: " << error << "\n";
+}
+
+static quark::SourceModule Parse() {
+  quark::LexContext ctx;
+  quark::SourceModule sm;
+
+  quark::QuarkParser quarkParser(ctx, sm);
+  quarkParser.parse();
+
+  return sm;
+}
+
+int main(int argc, const char *argv[]) {
+  llvm::InitLLVM llvm(argc, argv);
+
+  llvm::StringRef file = argv[1];
+  std::ifstream f(file.data());
+
+  freopen(file.data(), "r", stdin);
+
+  SourceModule sm = Parse();
+  sm.dump();
 
   return 0;
 }
